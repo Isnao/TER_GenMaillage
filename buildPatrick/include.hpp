30c2053292131db45d6746029f57c21d7c7e3a1d
dedfynx <sio.raynaud@gmail.com>
2020-04-27 15:04:37 +0200

    Add files via upload
    Oublis des #include

--- buildPatrick/include.hpp
+++ buildPatrick/include.hpp
@@ -1,3 +1,21 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Triangulation_3.h>
+#include <CGAL/Mesh_triangulation_3.h>
+#include <CGAL/Mesh_complex_3_in_triangulation_3.h>
+#include <CGAL/Mesh_criteria_3.h>
+
+#include <map>
+#include <iostream>
+//#include <CGAL/Labeled_mesh_domain_3.h>
+#include <CGAL/Labeled_image_mesh_domain_3.h>
+#include <CGAL/make_mesh_3.h>
+#include <CGAL/Image_3.h>
+
+#include <CGAL/Mesh_3/Mesh_global_optimizer.h>
+#include <CGAL/Mesh_domain_with_polyline_features_3.h>
+#include <CGAL/circulator.h>
+
 // Domain
 struct K: public CGAL::Exact_predicates_inexact_constructions_kernel {};
 typedef CGAL::Image_3 Image;
